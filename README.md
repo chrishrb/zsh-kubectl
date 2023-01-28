88bc0ee940a2e45539ef2f99c6f41a85d1951195
Christoph Herb <ch.herb@gmx.de>
2023-01-28 22:55:17 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# zsh-kubectl
+
+A zsh plugin for the [kubectl cli](https://github.com/kubernetes/kubectl).
+
+## Install
+Using the [:zap: Zap](https://www.zapzsh.org/) minimal zsh plugin manager, add the following to your `.zshrc`
+
+```sh
+# Install plugin
+plug "chrishrb/zsh-kubectl"
+```
+
+## License
+The **zsh-kubectl** plugin is released under the [Apache License 2.0](https://github.com/chrishrb/zsh-kubectl/blob/main/LICENSE) unless explicitly mentioned in the file header.
---
+++ zsh-kubectl.plugin.zsh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+
+if (( ! $+commands[kubectl] )); then
+  return
+fi
+
+local COMPLETIONS_DIR="${0:A:h}/completions"
+
+# Only regenerate completions if does not exist
+if [[ ! -f "$COMPLETIONS_DIR/_kubectl" ]]; then
+  kubectl completion zsh 2> /dev/null >| "$COMPLETIONS_DIR/_kubectl" &|
+fi
+
+# This command is used a LOT both below and in daily life
+alias k=kubectl
+
+# Add completions to the FPATH
+typeset -TUx FPATH fpath
+fpath=("$COMPLETIONS_DIR" $fpath)
