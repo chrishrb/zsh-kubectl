2bc5fa79c1b1f905f67e8273c68c2aa088ca7ef7
Christoph Herb <ch.herb@gmx.de>
2023-10-22 22:36:19 +0200

    fix kubectl autocompletion

--- zsh-kubectl.plugin.zsh
+++ zsh-kubectl.plugin.zsh
@@ -9,21 +9,11 @@ alias k=kubectl
 
 local COMPLETIONS_DIR="${0:A:h}/completions"
 
-# Placeholder 'kubectl' shell function:
-# Will only be executed on the first call to 'kubectl'
-kubectl() {
-  # Remove this function, subsequent calls will execute 'kubectl' directly
-  unfunction "$0"
+# Only regenerate completions if does not exist or older than 24 hours
+if [[ ! -f "$COMPLETIONS_DIR/_kubectl" || ! $(find "$COMPLETIONS_DIR/_kubectl" -newermt "24 hours ago" -print) ]]; then
+  kubectl completion zsh 2> /dev/null >| "$COMPLETIONS_DIR/_kubectl" &|
+fi
 
-  # Only regenerate completions if does not exist or older than 24 hours
-  if [[ ! -f "$COMPLETIONS_DIR/_kubectl" || ! $(find ~/.local/share/zap/plugins/zsh-kubectl/completions/_kubectl -newermt "24 hours ago" -print) ]]; then
-    kubectl completion zsh 2> /dev/null >| "$COMPLETIONS_DIR/_kubectl" &|
-  fi
-
-  # Add completions to the FPATH
-  typeset -TUx FPATH fpath
-  fpath=("$COMPLETIONS_DIR" $fpath)
-
-  # Execute 'kubectl' binary
-  $0 "$@"
-}
+# Add completions to the FPATH
+typeset -TUx FPATH fpath
+fpath=("$COMPLETIONS_DIR" $fpath)
